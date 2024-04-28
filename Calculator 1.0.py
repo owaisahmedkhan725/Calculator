cbcd20ed3e48b3cce02e55514ec48ee688324d91
Owais Ahmed Khan <154299423+owaisahmedkhan725@users.noreply.github.com>
2024-04-28 14:25:07 +0500

    Calculator v-1.0
    First Version Release

---
+++ Calculator 1.0.py
@@ -0,0 +1,32 @@
+# Author: github.com/owaisahmedkhan725
+
+# Calculator v-1.0
+# Basic Static Calculator.
+# Static Answers of All Possible Calculations.
+
+print () # For space
+
+print("               Calculator      ")
+
+x=5 # Variable
+y=6 # Variable
+
+print()
+print("x = ",x) # Print value of x
+print("y = ",y) # Print value of y
+
+print()
+#These Functions Perform Calculations.
+a=x+y
+print(x,"+",y,"=",a)
+
+b=x-y
+print(x,"-",y,"=",b)
+
+c=x*y
+print(x,"*",y,"=",c)
+
+d=x/y
+print(x,"/",y,"=",d)
+
+print()
