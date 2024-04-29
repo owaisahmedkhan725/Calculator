66508bb1e855aad07be1d4c93cefed83904d6bb1
owaisahmedkhan725 <owaisahmedkhan725@gmail.com>
2024-04-29 16:10:33 +0500

    Calculator v-1.1

---
+++ Calculator 1.1.py
@@ -0,0 +1,28 @@
+# Author: github.com/owaisahmedkhan725
+
+# Calculator v-1.1
+# Basic Calculator
+# Answers to All possible Calculations of given values at Once.
+# In This Update, You Can Take Input From the User.
+
+print()
+print("               Calculator      ")
+print ()
+x=int(input("Enter Value 1: ")) # Take value from user(x)
+y=int(input("Enter Value 2: ")) # Take value from user(y)
+
+print()
+# These are Functions
+a=x+y
+print(x,"+",y,"=",a)
+
+b=x-y
+print(x,"-",y,"=",b)
+
+c=x*y
+print(x,"*",y,"=",c)
+
+d=x/y
+print(x,"/",y,"=",d)
+
+print()
